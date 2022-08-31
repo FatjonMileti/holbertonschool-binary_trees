41422a2240470340252f4003d1a12a99384475f0
armigjoka <armigjoka090@gmail.com>
2022-08-31 03:49:00 -0700

    task 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include "binary_trees.h"
 /**
- *
+ * binary_tree_delete - function that deletes an entire binary tree
+ * @tree: pointer to the root node of a tree to delete
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
