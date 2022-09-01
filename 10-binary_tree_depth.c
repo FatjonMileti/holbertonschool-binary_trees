17c529cb77299ca7ab1e3297ef322759562bca8e
FatjonMileti <tonimileti@yahoo.com>
2022-09-01 00:59:28 -0700

    ...

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+/**
+ *
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (!tree || !tree->parent)
+		return (0);
+	return (binary_tree_depth(tree->parent) + 1);
+}
