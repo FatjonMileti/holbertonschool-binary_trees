a3eb7e199cb4deed28e3850fe8e6d7f2a45c7e94
armigjoka <armigjoka090@gmail.com>
2022-09-01 02:14:00 -0700

    task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -23,7 +23,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 }
 
 /**
- *
+ * binary_tree_balance - function that measures the balance factor
+ * of binary tree
+ * @tree: pointer to the root node of the tree
+ * Return: 0
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
