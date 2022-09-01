35082e3905c4acde392f1259a03a2f5c971cd00a
armigjoka <armigjoka090@gmail.com>
2022-09-01 01:54:21 -0700

    task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -2,7 +2,10 @@
 #include <stdio.h>
 #include "binary_trees.h"
 /**
- *
+ * binary_tree_nodes - function that couts the nodes with at least
+ * 1 child in a binary tree
+ * @tree: the pointer tto the root node of the tree
+ * Return: 0
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
